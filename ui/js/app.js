fd48fad26e000491cf0bc6832f7e459f32146dec
Bombay <72895600+BombayV@users.noreply.github.com>
2022-02-05 12:43:12 -0800

    tweak(ui/js): move value measure to if statement

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,23 +1,22 @@
 window.addEventListener('load', _ => {
-    let value = 0;
-    let counter = 0;
     const loading = document.getElementById('loading-bar');
     const myAudio = document.getElementById('music');
+    let value = 0;
+    let counter = 0;
     myAudio.volume = 0.1;
 
     window.addEventListener('message', function(e) {
         switch (e.data.eventName) {
             case 'initFunctionInvoking':
                 counter++
-                value = (counter * 100) / 224
                 break;
-            case 'performMapLoadFunction':+
+            case 'performMapLoadFunction':
                 counter++
-                value = (counter * 100) / 224
                 break;
         }
-        loading.style.opacity = `${value / 100}`
         if (value <= 100) {
+            value = (counter * 100) / 224;
+            loading.style.opacity = `${value / 100}`;
             loading.style.width = `${value}%`;
         }
     });
