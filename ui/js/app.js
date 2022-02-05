fa7151c87058e3f62f1c0336f27783aa418cab49
Olvidate <olvidatebooking@gmail.com>
2022-02-05 14:09:36 -0300

    fix(ui): correct calc to find value

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,36 +1,19 @@
-
-
 window.addEventListener('load', _ => {
     let value = 0;
+    let counter = 0;
     const loading = document.getElementById('loading-bar');
     const myAudio = document.getElementById('music');
     myAudio.volume = 0.1;
 
     window.addEventListener('message', function(e) {
         switch (e.data.eventName) {
-            case 'startInitFunction':
-                value = value + 1/10
-                break;
-            case 'startInitFunctionOrder':
-                value = value + 1/10
-                break;
             case 'initFunctionInvoking':
-                value = value + 1/10
+                counter++
+                value = (counter * 100) / 224
                 break;
-            case 'initFunctionInvoked':
-                value = value + 1/10
-                break;
-            case 'endInitFunction':
-                value = value + 1/10
-                break;
-            case 'startDataFileEntries':
-                value = value + 1/10
-                break;
-            case 'onDataFileEntry':
-                value = value + 1/10
-                break;
-            case 'endDataFileEntries':
-                value = value + 1/10
+            case 'performMapLoadFunction':+
+                counter++
+                value = (counter * 100) / 224
                 break;
         }
         loading.style.opacity = `${value / 100}`
@@ -39,4 +22,3 @@ window.addEventListener('load', _ => {
         }
     });
 })
-
