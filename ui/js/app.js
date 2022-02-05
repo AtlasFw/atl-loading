383d4d7a8021d2acb22d4446209397e1724c809a
Bombay <72895600+BombayV@users.noreply.github.com>
2022-02-05 12:45:47 -0800

    feat(ui/js): load based on progress (#1)
    * feat(js): added event switch
    
    * feat(ui/js): added testing values
    
    * fix(ui): correct calc to find value
    
    * tweak(ui/js): move value measure to if statement
    
    Co-authored-by: Olvidate <olvidatebooking@gmail.com>

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,33 +1,23 @@
-let value = 0;
-const sidebar = document.getElementById('loading-bar');
+window.addEventListener('load', _ => {
+    const loading = document.getElementById('loading-bar');
+    const myAudio = document.getElementById('music');
+    let value = 0;
+    let counter = 0;
+    myAudio.volume = 0.1;
 
-const updateProgress = () => {
-  sidebar.style.width = `${value}%`
-}
-
-const handlers = {
-  startInitFunctionOrder(data)
-  {
-    value = 25;
-  },
-
-  initFunctionInvoking(data)
-  {
-    value = 50;
-  },
-
-  startDataFileEntries(data)
-  {
-    value = 75;
-  },
-
-  performMapLoadFunction(data)
-  {
-    value = 100;
-  },
-};
-
-window.addEventListener('message', function(e)
-{
-  (handlers[e.data.eventName] || function() {})(e.data);
-});
+    window.addEventListener('message', function(e) {
+        switch (e.data.eventName) {
+            case 'initFunctionInvoking':
+                counter++
+                break;
+            case 'performMapLoadFunction':
+                counter++
+                break;
+        }
+        if (value <= 100) {
+            value = (counter * 100) / 224;
+            loading.style.opacity = `${value / 100}`;
+            loading.style.width = `${value}%`;
+        }
+    });
+})
