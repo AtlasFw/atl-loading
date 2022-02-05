fb8e8aa66a8f57c037500438de592e63582a26eb
Olvidate <olvidatebooking@gmail.com>
2022-02-04 21:56:45 -0300

    feat(ui): added progress to loadscreen

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,6 +1,33 @@
+let value = 0;
 const sidebar = document.getElementById('loading-bar');
 
 const updateProgress = () => {
-  const value = 100
   sidebar.style.width = `${value}%`
 }
+
+const handlers = {
+  startInitFunctionOrder(data)
+  {
+    value = 25;
+  },
+
+  initFunctionInvoking(data)
+  {
+    value = 50;
+  },
+
+  startDataFileEntries(data)
+  {
+    value = 75;
+  },
+
+  performMapLoadFunction(data)
+  {
+    value = 100;
+  },
+};
+
+window.addEventListener('message', function(e)
+{
+  (handlers[e.data.eventName] || function() {})(e.data);
+});
