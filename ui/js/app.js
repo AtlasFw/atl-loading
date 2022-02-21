edf2dd54761e7ff0784a057c8bfb62b060fc3f0b
BombayV <OMGmsebastian@gmail.com>
2022-02-21 11:17:04 -0500

    feat(ui): replace for mesh and remove loading bar

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,23 +1,14 @@
 window.addEventListener('load', _ => {
-    const loading = document.getElementById('loading-bar');
-    const myAudio = document.getElementById('music');
-    let value = 0;
+    document.getElementById('music').volume = 0.1;
     let counter = 0;
-    myAudio.volume = 0.1;
-
-    window.addEventListener('message', function(e) {
-        switch (e.data.eventName) {
-            case 'initFunctionInvoking':
-                counter++
-                break;
-            case 'performMapLoadFunction':
-                counter++
-                break;
-        }
-        if (counter <= 224) {
-            value = (counter * 100) / 224;
-            loading.style.opacity = `${value / 100}`;
-            loading.style.width = `${value}%`;
+    window.addEventListener('message', (e) => {
+        if (e.data.eventName == 'endInitFunction') {
+            counter++
+            if (counter == 2) {
+                setTimeout(() => {
+                    document.getElementById('app').style.opacity = '0';
+                }, 5000);
+            }
         }
     });
 })
