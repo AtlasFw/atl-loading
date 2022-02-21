7ebf8d17ab572dbd040c704a1db8130669dfcd0d
BombayV <OMGmsebastian@gmail.com>
2022-02-21 14:59:15 -0500

    feat(js): added counter back

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,8 +1,13 @@
 window.addEventListener('load', _ => {
     document.getElementById('music').volume = 0.1;
+    let counter = 0;
     window.addEventListener('message', (e) => {
+        // Needs to be tested because atm it's not smooth
         if (e.data.eventName == 'endInitFunction') {
-            document.getElementById('app').style.opacity = '0';
+            counter++
+            if (counter == 2) {
+                document.getElementById('app').style.opacity = '0';
+            }
         }
     });
 })
