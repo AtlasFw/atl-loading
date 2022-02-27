77c0c258820145e0df809a0ed836f7794b9f6f63
LittleFishy <89689629+LittleFishyy@users.noreply.github.com>
2022-02-27 21:46:23 +0100

    feat(js): Added stop and start music

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', _ => {
     document.getElementById('music').volume = 0.1;
     let counter = 0;
+    let stoped = false;
     window.addEventListener('message', (e) => {
         // Needs to be tested because atm it's not smooth
         if (e.data.eventName == 'endInitFunction') {
@@ -10,4 +11,16 @@ window.addEventListener('load', _ => {
             }
         }
     });
+    
+    document.body.onkeyup = function(e){
+        if(e.keyCode == 32){
+            if (stoped == false){
+                document.getElementById('music').volume = 0;
+                stoped = true;
+            }else if(stoped == true){
+                document.getElementById('music').volume = 0.1;
+                stoped = false;
+            }
+        }
+    };
 })
