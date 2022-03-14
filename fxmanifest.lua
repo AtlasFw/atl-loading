f9e29e22b5dd0baac7950c314447648a9dc634ec
German Fernandez <germanfernandez0306@gmail.com>
2022-03-14 20:01:36 -0300

    remove comment

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,13 +5,13 @@ name 'atl-loading'
 author 'Atlas Framework Developers'
 version '0.0.1'
 
-loadscreen 'ui/ui.html'
+loadscreen 'web/index.html'
 loadscreen_manual_shutdown 'yes'
 
 files {
-  'ui/ui.html',
-  'ui/js/app.js',
-	'ui/css/main.css'
+  'web/index.html',
+  'web/js/main.js',
+	'web/css/main.css'
 }
 
 provide 'loading'
---
+++ web/js/main.js
@@ -0,0 +1,17 @@
+const video = document.getElementById('video');
+let counter = 0;
+window.addEventListener('message', (e) => {
+  // Needs to be tested because atm it's not smooth. Maybe there is another event that works better.
+  if (e.data.eventName === 'endInitFunction') {
+    // counter++
+    // if (counter == 2) {
+    //   document.getElementById('app').style.opacity = '0';
+    // }
+  }
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.code === 'Space') {
+    video.muted ? (video.muted = false) : (video.muted = true);
+  }
+});
